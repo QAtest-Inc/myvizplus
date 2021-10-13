71d60b093f2fe489b0ad722e28e73b012c170182
QAtest Inc <89322162+qause@users.noreply.github.com>
2021-10-13 07:08:15 +0000

    develop 0.1.1

--- my-app/cypress/plugins/index.js
+++ my-app/cypress/plugins/index.js
@@ -1,62 +1,9 @@
-const injectDevServer = require('@cypress/react/plugins/babel');
+// cypress/plugins/index.js
 
-class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      text: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const injectDevServer = require('@cypress/react/plugins/react-scripts')
 
-  render() {
-    return React.createElement("div", null, React.createElement("h3", null, "TODO"), React.createElement(TodoList, {
-      items: this.state.items
-    }), React.createElement("form", {
-      onSubmit: this.handleSubmit
-    }, React.createElement("label", {
-      htmlFor: "new-todo"
-    }, "What needs to be done?"), React.createElement("input", {
-      id: "new-todo",
-      onChange: this.handleChange,
-      value: this.state.text
-    }), React.createElement("button", null, "Add #", this.state.items.length + 1)));
-  }
+module.exports = (on, config) => {
+  injectDevServer(on, config)
 
-  handleChange(e) {
-    this.setState({
-      text: e.target.value
-    });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-
-    if (this.state.text.length === 0) {
-      return;
-    }
-
-    const newItem = {
-      text: this.state.text,
-      id: Date.now()
-    };
-    this.setState(state => ({
-      items: state.items.concat(newItem),
-      text: ''
-    }));
-  }
-
+  return config
 }
-
-class TodoList extends React.Component {
-  render() {
-    return React.createElement("ul", null, this.props.items.map(item => React.createElement("li", {
-      key: item.id
-    }, item.text)));
-  }
-
-}
-
-ReactDOM.render(React.createElement(TodoApp, null), document.getElementById('todos-example'));
