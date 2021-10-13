7582743cc22a5e3ba007731555cb562206b9aaa4
QAtest Inc <89322162+qause@users.noreply.github.com>
2021-10-13 06:06:41 +0000

    develop 0.1.0

--- my-app/cypress/plugins/index.js
+++ my-app/cypress/plugins/index.js
@@ -1,53 +1,43 @@
+const injectDevServer = require('@cypress/react/plugins/babel');
+
 class TodoApp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { items: [], text: '' };
+    this.state = {
+      items: [],
+      text: ''
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   render() {
-    return React.createElement(
-      "div",
-      null,
-      React.createElement(
-        "h3",
-        null,
-        "TODO"
-      ),
-      React.createElement(TodoList, { items: this.state.items }),
-      React.createElement(
-        "form",
-        { onSubmit: this.handleSubmit },
-        React.createElement(
-          "label",
-          { htmlFor: "new-todo" },
-          "What needs to be done?"
-        ),
-        React.createElement("input", {
-          id: "new-todo",
-          onChange: this.handleChange,
-          value: this.state.text
-        }),
-        React.createElement(
-          "button",
-          null,
-          "Add #",
-          this.state.items.length + 1
-        )
-      )
-    );
+    return React.createElement("div", null, React.createElement("h3", null, "TODO"), React.createElement(TodoList, {
+      items: this.state.items
+    }), React.createElement("form", {
+      onSubmit: this.handleSubmit
+    }, React.createElement("label", {
+      htmlFor: "new-todo"
+    }, "What needs to be done?"), React.createElement("input", {
+      id: "new-todo",
+      onChange: this.handleChange,
+      value: this.state.text
+    }), React.createElement("button", null, "Add #", this.state.items.length + 1)));
   }
 
   handleChange(e) {
-    this.setState({ text: e.target.value });
+    this.setState({
+      text: e.target.value
+    });
   }
 
   handleSubmit(e) {
     e.preventDefault();
+
     if (this.state.text.length === 0) {
       return;
     }
+
     const newItem = {
       text: this.state.text,
       id: Date.now()
@@ -57,20 +47,16 @@ class TodoApp extends React.Component {
       text: ''
     }));
   }
+
 }
 
 class TodoList extends React.Component {
   render() {
-    return React.createElement(
-      "ul",
-      null,
-      this.props.items.map(item => React.createElement(
-        "li",
-        { key: item.id },
-        item.text
-      ))
-    );
+    return React.createElement("ul", null, this.props.items.map(item => React.createElement("li", {
+      key: item.id
+    }, item.text)));
   }
+
 }
 
 ReactDOM.render(React.createElement(TodoApp, null), document.getElementById('todos-example'));
